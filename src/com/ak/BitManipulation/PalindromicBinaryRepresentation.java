59736cf0eadc821112ade8230e76db418153df2d
AMBAR KANSAL <75237568+Ambarcode@users.noreply.github.com>
2023-07-02 02:30:35 +0530

    optimized approach added

--- src/com/ak/BitManipulation/PalindromicBinaryRepresentation.java
+++ src/com/ak/BitManipulation/PalindromicBinaryRepresentation.java
@@ -35,11 +35,40 @@ public class PalindromicBinaryRepresentation {
                 return sb.toString().equals(toBinaryString);
         }
 
-        //Now let's see a optimized approach
+        //Now let's see an optimized approach
+        private static int findNthPalindromicBinary(int n) {
+                int count = 1;
+                int len = 1;
 
+                while (count < n) {
+                        len++;
+                        int elemAtThisPos = (1 << ((len - 1) / 2));
+                        count += elemAtThisPos;
+                }
+                count-=(1 << ((len - 1) / 2));
+                int offset=n-count-1;
+                int ans=1<<len-1;
+                ans|=(offset<<len-2);
+
+                int valPR = (ans >> (len / 2));
+                int rev = reverse(valPR);
+
+                ans |= rev;
+                return ans;
+        }
+        public static int reverse(int number) {
+                int reversed = 0;
+                int temp = number;
+
+                while (temp > 0) {
+                        reversed = (reversed << 1) | (temp & 1);
+                        temp >>= 1;
+                }
+                return reversed;
+        }
 
         public static void main(String[] args) {
-                System.out.println(solve(2000));
+                System.out.println(Integer.toBinaryString(findNthPalindromicBinary(2000)));
         }
 
 }
